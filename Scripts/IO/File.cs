f76259c40528ff99e35a14a57c52feb3cd5c3824
ConfiG <max1m1232@yandex.ru>
2020-08-22 17:13:59 +0300

    Make the PPR.Scripts.IO.File path be relative to the level path

--- Scripts/IO/File.cs
+++ Scripts/IO/File.cs
@@ -2,28 +2,31 @@
 using System.IO;
 using System.Text;
 
+using PPR.GUI;
+using PPR.Main.Levels;
+
 namespace PPR.Scripts.IO {
     public static class File {
         public static FileStream OpenRead(string path) {
-            return System.IO.File.OpenRead(path);
+            return System.IO.File.OpenRead(Path.Combine("levels", Map.currentLevel.metadata.name, path));
         }
         public static string[] ReadAllLines(string path) {
-            return System.IO.File.ReadAllLines(path);
+            return System.IO.File.ReadAllLines(Path.Combine("levels", Map.currentLevel.metadata.name, path));
         }
         public static string[] ReadAllLines(string path, Encoding encoding) {
-            return System.IO.File.ReadAllLines(path, encoding);
+            return System.IO.File.ReadAllLines(Path.Combine("levels", Map.currentLevel.metadata.name, path), encoding);
         }
         public static string ReadAllText(string path) {
-            return System.IO.File.ReadAllText(path);
+            return System.IO.File.ReadAllText(Path.Combine("levels", Map.currentLevel.metadata.name, path));
         }
         public static string ReadAllText(string path, Encoding encoding) {
-            return System.IO.File.ReadAllText(path, encoding);
+            return System.IO.File.ReadAllText(Path.Combine("levels", Map.currentLevel.metadata.name, path), encoding);
         }
         public static IEnumerable<string> ReadLines(string path) {
-            return System.IO.File.ReadLines(path);
+            return System.IO.File.ReadLines(Path.Combine("levels", Map.currentLevel.metadata.name, path));
         }
         public static IEnumerable<string> ReadLines(string path, Encoding encoding) {
-            return System.IO.File.ReadLines(path, encoding);
+            return System.IO.File.ReadLines(Path.Combine("levels", Map.currentLevel.metadata.name, path), encoding);
         }
     }
 }
