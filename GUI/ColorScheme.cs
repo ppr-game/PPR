b4fe8fe143a2580551b3a0fe13f1d11311d675af
ConfiG <max1m1232@yandex.ru>
2020-09-21 20:12:25 +0300

    Fix color schemes loading order

--- GUI/ColorScheme.cs
+++ GUI/ColorScheme.cs
@@ -35,48 +35,77 @@ namespace PPR.GUI {
                 File.ReadAllText(Path.Join("resources", "bloom_vert.glsl")), null,
                 File.ReadAllText(bloomBlendShaderPath));
             
-            Dictionary<string, string> queue = new Dictionary<string, string>();
-            
-            LoadScheme(Path.Join("Default", "Classic"), queue);
-            LoadScheme(Settings.GetPath("colorScheme"), queue);
-            
-            LoadQueue(queue);
+            LoadSchemes(Path.Join("Default", "Classic"), Settings.GetPath("colorScheme"));
 
             Game.UpdateSettings();
         }
 
-        static void LoadScheme(string path, IDictionary<string, string> queue) {
-            string filePath = Path.Join("resources", "colors", path, "colors.txt");
-            if(!File.Exists(filePath)) {
-                logger.Warn($"The color scheme at '{path}' was not found, falling back to default");
-                filePath = Path.Join("resources", "colors", "Default", "Classic", "colors.txt");
+        static void LoadSchemes(string basePath, string priorityPath) {
+            // Get the paths
+            string baseFilePath = Path.Join("resources", "colors", basePath, "colors.txt");
+            string priorityFilePath = Path.Join("resources", "colors", priorityPath, "colors.txt");
+            if(!File.Exists(priorityFilePath)) {
+                logger.Warn($"The color scheme at '{priorityPath}' was not found, falling back to base.");
+                priorityFilePath = baseFilePath;
             }
-            if(!File.Exists(filePath)) {
-                FileNotFoundException ex = new FileNotFoundException("The default color scheme was not found");
+            if(!File.Exists(baseFilePath)) {
+                FileNotFoundException ex = new FileNotFoundException("The base color scheme was not found.");
                 logger.Fatal(ex);
                 throw ex;
             }
-            string[] lines = File.ReadAllLines(filePath);
+            string[] baseLines = File.ReadAllLines(baseFilePath);
+            string[] priorityLines = File.ReadAllLines(priorityFilePath);
             
-            foreach(string line in lines) {
-                string usableLine = line.Split('#')[0].Replace(' ', '\0');
-                string[] keyValue = usableLine.Split('=');
-                if(keyValue.Length != 2) continue;
+            Dictionary<string, string> tempColors = new Dictionary<string, string>();
+            
+            // Save the base color scheme values in a dictionary
+            foreach(string line in baseLines) {
+                string[] keyValue = ParseSchemeLine(line);
+                if(keyValue == null) continue;
+                if(keyValue.Length != 2) {
+                    logger.Warn($"The base line '{line}' could not be parsed.");
+                    continue;
+                }
                 string key = keyValue[0];
                 string value = keyValue[1];
-                if(string.IsNullOrEmpty(key.Trim()) || string.IsNullOrEmpty(value.Trim())) continue;
-                string[] strValues = value.Split(',');
-                byte[] values = strValues.Length == 3 || strValues.Length == 4 ?
-                    strValues.Select(byte.Parse).ToArray() :
-                    Array.Empty<byte>();
-                if(values.Length == 3 || values.Length == 4)
-                    colors[key] = new Color(values[0], values[1], values[2],
-                        values.Length == 4 ? values[3] : byte.MaxValue);
-                else queue[key] = value;
+                tempColors[key] = value;
+            }
+            // Apply the priority overrides to the dictionary
+            foreach(string line in priorityLines) {
+                string[] keyValue = ParseSchemeLine(line);
+                if(keyValue == null) continue;
+                if(keyValue.Length != 2) {
+                    logger.Warn($"The priority line '{line}' could not be parsed.");
+                    continue;
+                }
+                string key = keyValue[0];
+                string value = keyValue[1];
+                tempColors[key] = value;
+            }
+            
+            // Parse and load the final dictionary as our color scheme
+            foreach((string key, string value) in tempColors) {
+                Color? color = ParseSchemeColor(value);
+                if(color == null) {
+                    if(tempColors.ContainsKey(value)) colors[key] = colors[value];
+                    else logger.Warn($"The color variable '{key} : {value}' could not be parsed.");
+                }
+                else colors[key] = (Color)color;
             }
         }
-        static void LoadQueue(Dictionary<string, string> queue) {
-            foreach((string key, string value) in queue) colors[key] = colors[value];
+        static string[] ParseSchemeLine(string line) {
+            string usableLine = line.Split('#')[0].Replace(' ', '\0');
+            string[] keyValue = usableLine.Split('=');
+            if(string.IsNullOrEmpty(keyValue[0].Trim()) || string.IsNullOrEmpty(keyValue[1].Trim())) return null;
+            return keyValue;
+        }
+        static Color? ParseSchemeColor(string color) {
+            string[] strValues = color.Split(',');
+            byte[] values = strValues.Length == 3 || strValues.Length == 4 ?
+                strValues.Select(byte.Parse).ToArray() : Array.Empty<byte>();
+            return strValues.Length == 3 || strValues.Length == 4 ? 
+                new Color(values[0], values[1], values[2],
+                values.Length == 4 ? values[3] : byte.MaxValue) : (Color?)null;
         }
 
         public static Color GetColor(string key) {
